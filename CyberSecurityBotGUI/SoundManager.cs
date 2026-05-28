87e7f64d54bf77a077f1c11e0ea1c131641520d0
kutlwano47 <st10469161@rcconnect.edu.za>
2026-05-28 12:30:10 +0200

    CHANGES3

---
+++ CyberSecurityBotGUI/SoundManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Media;
+
+namespace CyberSecurityBotGUI.Classes
+{
+    public class SoundManager
+    {
+        public void PlayGreeting()
+        {
+            SoundPlayer player = new SoundPlayer("welcome.wav");
+
+            player.Play();
+        }
+    }
+}
